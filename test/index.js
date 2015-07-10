920aa27eb7c7e6b558876975b25cc8bf530338c2
davidpetri <petridw@gmail.com>
2015-07-10 11:26:56 -0700

    (feat) tests are more general. prevent alert dialogs

--- test/index.js
+++ test/index.js
@@ -1,9 +1,12 @@
 describe('Snake', function() {
 
   var SPEED;
-  // hopefully all the borders are the same
-  var BORDER_WIDTH = parseInt($('#board').css("border-left-width"));
-  console.log(BORDER_WIDTH);
+  var BORDER_WIDTH = parseInt($('#board').css("border-left-width"), 10);
+  
+  // prevent alert dialogs
+  window.alert = function() {
+    console.log.apply(console, arguments);
+  };
 
   function move(direction) {
     var keyCodes = { 'left': 37, 'up': 38, 'right': 39, 'down': 40 };
@@ -12,21 +15,20 @@ describe('Snake', function() {
   }
 
   beforeEach(function() {
-    clock = sinon.useFakeTimers();
-
-    head.node.detach();
-
     board = $('#board');
-    head = new Head(board);
     SPEED = head.SPEED;
 
     // place head in middle of board
-    var position = head.node.position();
-    head.node.offset({left: (position.left + 300), top: (position.top + 300)});
+    var midX = board.position().left + BORDER_WIDTH + Math.floor(board.width() / 2) - 50;
+    var midY = board.position().top + BORDER_WIDTH + Math.floor(board.height() / 2) - 50;
+    head.node.offset({ left: midX, top: midY });
+
+    // start head moving right
+    move('right');
   });
 
   afterEach(function() {
-    clock.restore();
+    // clock.restore();
   });
 
   describe('Head', function() {
@@ -73,6 +75,17 @@ describe('Snake', function() {
     });
 
     describe('Game ends if head moves out of the board', function() {
+
+      afterEach(function() {
+        // place head in middle of board
+        var midX = board.position().left + BORDER_WIDTH + Math.floor(board.width() / 2) - 50;
+        var midY = board.position().top + BORDER_WIDTH + Math.floor(board.height() / 2) - 50;
+        head.node.offset({ left: midX, top: midY });
+
+        // get head moving again
+        head.currentDirection = 'up';
+        head.move();
+      });
 
       it('should not be able to move outside of the top of the board', function() {
         var oldPosition = head.node.position();
@@ -221,26 +234,36 @@ describe('Snake', function() {
       }
     });
 
-    it('should remove the apple when the head of the snake reaches the apple', function() {
+    it('should remove the apple and regenerate at a new location when the head of the snake reaches the apple', function() {
       var applePosition = apple.node.position();
+      var newPosition;
 
-      // make sure apple isn't on left wall
-      while (applePosition.left === board.position().left + BORDER_WIDTH) {
-        apple = new Apple(board);
-        applePosition = apple.node.position();
+      // make sure apple not moving right/left
+      move('down');
+
+      // if apple on left wall
+      if (applePosition.left === board.position().left + BORDER_WIDTH) {
+        newPosition = {
+          left: applePosition.left + 50,
+          top: applePosition.top
+        };
+        head.node.offset(newPosition);
+        move('left');
+      } else {
+        // place head to left of apple
+        newPosition = {
+          left: applePosition.left - 50,
+          top: applePosition.top
+        };
+        head.node.offset(newPosition);
+        move('right');
       }
 
-      // place head to left of apple
-      var newPosition = {
-        left: applePosition.left - 50,
-        top: applePosition.top
-      };
-      head.node.offset(newPosition);
-
-      move('right');
       var newApple = $('#apple');
+      expect(newApple.length).to.be.greaterThan(0);
       expect(newApple.position()).to.not.eql(applePosition);
     });
+
   });
 });
 mocha.globals();
