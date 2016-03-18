b4bdc8fbf76d886837e58edadab5df74fcd07381
Andrew Carlson <acarl005@g.ucla.edu>
2016-03-18 11:13:06 -0700

    refactor with class keyword

--- src/Head.js
+++ src/Head.js
@@ -1,21 +1,22 @@
-function Head($el) {
-  this.node = $('<div id="head"></div>');
-  this.currentDirection = 'right';
-  this.SPEED = 500;
-  $el.append(this.node);
-  this.node.css({top: 0, left: 0});
+class Head { // creates a constructor function
 
-  setTimeout(this.move.bind(this), this.SPEED);
-
-}
-
-Head.prototype.move = function() {
-  var direction = this.currentDirection;
-  var position = this.node.position();
-  if (direction === 'right') {
-    position.left += 50;
+  constructor($el) { // this is what's called when you use the "new" keyword
+    this.node = $('<div id="head"></div>');
+    this.currentDirection = 'right';
+    this.SPEED = 500;
+    $el.append(this.node);
+    this.node.css({ top: 0, left: 0 });
+    setTimeout(this.move.bind(this), this.SPEED);
   }
-  this.node.offset(position);
-  setTimeout(this.move.bind(this), this.SPEED);
+
+  move() {  // same as Head.prototype.move = function() {...}
+    var direction = this.currentDirection;
+    var position = this.node.position();
+    if (direction === 'right') {
+      position.left += 50;
+    }
+    this.node.offset(position);
+    setTimeout(this.move.bind(this), this.SPEED);
+  }
 
 }
