6cb93d125e07e37e07d0e1d453a15cd272569b21
Andrew Carlson <acarl005@g.ucla.edu>
2016-03-18 11:13:06 -0700

    refactor with class keyword

--- src/Apple.js
+++ src/Apple.js
@@ -1,6 +1,10 @@
-function Apple($el) {
-  this.node = $('<img id="apple"></img>');
-  this.node.attr('src', 'src/assets/apple.jpg');
-  $el.append(this.node);
-  this.node.css({top: 0, left: 0});
+class Apple {
+
+  constructor($el) {
+    this.node = $('<img id="apple"></img>');
+    this.node.attr('src', 'src/assets/apple.jpg');
+    $el.append(this.node);
+    this.node.css({ top: 0, left: 0 });
+  }
+
 }
